3d2692cbfdd64b23ddb91bf3f7d89ea05bb35666
pengzzzccc <PengZhanci2005@gmail.com>
2024-11-19 16:43:46 +1100

    commit

--- Music Action/Assets/Script/audioMove.cs
+++ Music Action/Assets/Script/audioMove.cs
@@ -12,6 +12,8 @@ public class audioMove : MonoBehaviour
 
     private float[] audioSamples;
     private Transform objectTransform;
+    private enum AudioStep{low, mid, hight}
+    [SerializeField] private AudioStep step;
 
     void Start()
     {
@@ -26,27 +28,72 @@ public class audioMove : MonoBehaviour
         }
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (material == null) return;
 
         // 获取音频频谱数据
         audioSource.GetSpectrumData(audioSamples, 0, FFTWindow.BlackmanHarris);
+        Debug.Log(audioSamples.Length);
 
-        // 计算平均音频幅度，映射为缩放比例
-        float averageLevel = 0;
-        foreach (float sample in audioSamples)
+        int range = audioSamples.Length/3;
+
+        switch(step)
         {
-            averageLevel += sample;
+            case AudioStep.low:
+                float lowaverageLevel = 0;
+                for (int i = 0;i < range ;i++)
+                {
+                    lowaverageLevel += audioSamples[i];
+                }
+                lowaverageLevel /= sampleDataLength * 0.2f;
+
+                // 设置对象缩放
+                objectTransform.localScale = Vector3.one * Mathf.Lerp((1 + lowaverageLevel * scaleMultiplier), objectTransform.localScale.magnitude, 0.5f);
+                light.intensity = lowaverageLevel * scaleMultiplier * 1.2f;
+                light.color = Color.red;
+
+                // 设置自发光颜色的强度
+                Color emissionColor = Color.red * (1 + lowaverageLevel * scaleMultiplier);
+                material.SetColor("_EmissionColor", emissionColor);
+                break;
+            case AudioStep.mid:
+                float midaverageLevel = 0;
+                for (int i = range;i < range * 2 ;i++)
+                {
+                    midaverageLevel += audioSamples[i];
+                }
+                midaverageLevel /= sampleDataLength * 0.2f;
+
+                // 设置对象缩放
+                objectTransform.localScale = Vector3.one * Mathf.Lerp((1 + midaverageLevel * scaleMultiplier), objectTransform.localScale.magnitude, 0.5f);
+                light.intensity = midaverageLevel * scaleMultiplier * 1.2f;
+                light.color = Color.green;
+
+                // 设置自发光颜色的强度
+                Color midemissionColor = Color.green * (1 + midaverageLevel * scaleMultiplier);
+                material.SetColor("_EmissionColor", midemissionColor);
+            break;
+            case AudioStep.hight:
+                float hitaverageLevel = 0;
+                for (int i = range * 2;i < audioSamples.Length ;i++)
+                {
+                    hitaverageLevel += audioSamples[i];
+                }
+                hitaverageLevel /= sampleDataLength * 0.2f;
+
+                // 设置对象缩放
+                objectTransform.localScale = Vector3.one * Mathf.Lerp((1 + hitaverageLevel * scaleMultiplier), objectTransform.localScale.magnitude, 0.5f);
+                light.intensity = hitaverageLevel * scaleMultiplier * 1.2f;
+                light.color = Color.yellow;
+
+                // 设置自发光颜色的强度
+                Color hitemissionColor = Color.yellow * (1 + hitaverageLevel * scaleMultiplier);
+                material.SetColor("_EmissionColor", hitemissionColor);
+            break;
         }
-        averageLevel /= sampleDataLength * 0.2f;
 
-        // 设置对象缩放
-        objectTransform.localScale = Vector3.one * (1 + averageLevel * scaleMultiplier);
-        light.intensity = averageLevel * scaleMultiplier * 1.2f;
 
-        // 设置自发光颜色的强度
-        Color emissionColor = Color.white * (1 + averageLevel * scaleMultiplier);  // 这里使用白色，可以更改为其他颜色
-        material.SetColor("_EmissionColor", emissionColor);  // 设置自发光颜色
+
     }
 }
